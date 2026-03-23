43a42786d0c739445cc53101fb21c7a3b0c7631f
SHARVAN223 <sharvan70458@gmail.com>
2026-03-24 00:33:18 +0530

    functional

--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from app.models import *
 
-class Stu1_serializer(serializers.Serializer):
-    Name = serializers.CharField(max_length=50)
-    Email = serializers.EmailField()
-    Age = serializers.IntegerField()
-    Contact = serializers.CharField(max_length=15)
-
-    def create(self,validated_data):
-        return Student1.objects.create(**validated_data)
+class Stu1_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student1
+        fields = '__all__'
+    
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from rest_framework.decorators import api_view
 from app.models import *
 from .serializers import *
@@ -19,33 +19,33 @@ def student(req):
     serializer = Stu1_serializer(data, many=True)
     return Response(serializer.data)
 
-@api_view(['PUT','PATCH','DELETE'])
+@api_view(['GET','PUT','PATCH','DELETE'])
 def student1(req,pk):
     user=Student1.objects.filter(id=pk)
     if user:
         if req.method == 'PUT':
-            data = Student1.objects.get(id=pk)
+            data = Student1.objects.get(pk=pk)
             serializer = Stu1_serializer(data, data=req.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         elif req.method == 'PATCH':
-            data = Student1.objects.get(id=pk)
+            data = Student1.objects.get(pk=pk)
             serializer = Stu1_serializer(data, data=req.data , partial=True)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         elif req.method == 'DELETE':
-            data = Student1.objects.get(id=pk)
+            data = Student1.objects.get(pk=pk)
             data.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        data = Student1.objects.all()
-        serializer = Stu1_serializer(data, many=True)
-        return Response(serializer.data)
-    return HttpResponse ({'msg':'id is not db'},content_type='application/json')
+        elif req.method == 'GET':
+            data = Student1.objects.all()
+            serializer = Stu1_serializer(data, many=True)
+            return Response(serializer.data)
+    return JsonResponse({'msg':'id is not db'},content_type='application/json')
     
     
 
